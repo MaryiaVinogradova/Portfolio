fbf56d4e22a2189724a5c6d475fe14f214b698df
Maryia Vinogradova <maryiavinogradova@Maryias-MacBook-Air.local>
2022-06-13 20:28:49 -0600

    test

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../styles/bio.css';
+
+
+function bio() {
+
+    return (
+        <div class="full-height">
+         <div id="bio" className="container">
+
+            <img id="image" alt="my portrait" src="https://github.com/MaryiaVinogradova/Portfolio/blob/main/src/images/portrait%20option%201.jpg?raw=true" />
+
+            <div className="main-content-div">
+                <p className="p-for-about-me">I am an architect with more than 8 years of experience. Looking for a new opportunity in IT as full-stack developper. 
+             
+                </p>
+            </div>
+         </div>
+        </div>
+    )
+
+}
+
+export default bio;
+
+
