a543e6717278f512d0e815d5765cd818bcfdabb7
Maryia Vinogradova <maryiavinogradova@Maryias-MacBook-Air.local>
2022-09-24 22:18:42 -0600

    final updates

---
+++ build/precache-manifest.508a6d6b0c2a05fe0841ef8571cb7c10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "98f2fb9714ef86110ba2",
+    "url": "/Portfolio/static/css/main.ae2ebbb8.chunk.css"
+  },
+  {
+    "revision": "98f2fb9714ef86110ba2",
+    "url": "/Portfolio/static/js/main.98f2fb97.chunk.js"
+  },
+  {
+    "revision": "d3586b13b66ce6e81d0c",
+    "url": "/Portfolio/static/css/1.a99e045b.chunk.css"
+  },
+  {
+    "revision": "d3586b13b66ce6e81d0c",
+    "url": "/Portfolio/static/js/1.d3586b13.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "48abd53be1e6247bfc5e79e1f3c5d00f",
+    "url": "/Portfolio/index.html"
+  }
+];
