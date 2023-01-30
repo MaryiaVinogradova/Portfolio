70d621d95768088aa591cf9d8b09eca8f5162bc6
Maryia Vinogradova <maryiavinogradova@Maryias-MacBook-Air.local>
2023-01-30 10:17:41 -0700

    added

---
+++ build/precache-manifest.df6625ee56e56f474a3c28b852258932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b50bb339cc42ac470769",
+    "url": "/Portfolio/static/css/main.51e6cd96.chunk.css"
+  },
+  {
+    "revision": "b50bb339cc42ac470769",
+    "url": "/Portfolio/static/js/main.b50bb339.chunk.js"
+  },
+  {
+    "revision": "7473bba1289452644cd8",
+    "url": "/Portfolio/static/css/1.a99e045b.chunk.css"
+  },
+  {
+    "revision": "7473bba1289452644cd8",
+    "url": "/Portfolio/static/js/1.7473bba1.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "2c0d92919eb447d3c377802b22610b4b",
+    "url": "/Portfolio/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.978964ed0dddcdd28a6f2eef25fe3c3d.js"
+  "/Portfolio/precache-manifest.df6625ee56e56f474a3c28b852258932.js"
 );
 
 workbox.clientsClaim();
